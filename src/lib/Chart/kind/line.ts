f1a8142d79004eec6608a54571d307cfb2c02709
Yann Amsellem <yann@agnostic.engineering>
2025-03-24 11:13:24 +0100

    fix(line): use tule and dot to display one tips on multiple lines

--- src/lib/Chart/kind/line.ts
+++ src/lib/Chart/kind/line.ts
@@ -3,7 +3,7 @@ import * as Plot from '@observablehq/plot';
 import d from '../utils/conf.js';
 import colors from '../utils/colors.js';
 
-import type { Data, Point } from '$lib/index.js';
+import type { Data, Point, Value } from '$lib/index.js';
 
 export default function line(
 	size: { width: number; height: number },
@@ -11,20 +11,55 @@ export default function line(
 	axis: { x: string; y: string[]; z?: string }
 ): Node {
 	return Plot.plot({
-		marks: axis.y.map((y, i) =>
-			Plot.line(data, {
-				x: (d: Point) => {
-					const val = d[axis.x];
-					const date = new Date(val as string | number);
-					return isNaN(date.getTime()) ? val : date;
-				},
-				y,
-				stroke: axis.z ?? colors[i],
-				tip: true
-			})
-		),
+		marks: [
+			...axis.y.map((y, i) =>
+				Plot.line(data, {
+					x: (d: Point) => {
+						const val = d[axis.x];
+						const date = new Date(val as string | number);
+						return isNaN(date.getTime()) ? val : date;
+					},
+					y,
+					stroke: axis.z ?? colors[i],
+					tip: axis.y.length === 1
+				})
+			),
+			...tip(data, axis)
+		],
 		width: size.width,
 		height: size.height,
 		...d
 	});
+}
+
+function tip(data: Data, axis: { x: string; y: string[]; z?: string }) {
+	if (axis.y.length <= 1) return [];
+
+	const tipsData = data.map((d) => ({
+		...d,
+		_y: avg(...axis.y.map((y) => d[y]))
+	}));
+
+	function x(d: Point) {
+		const val = d[axis.x];
+		const date = new Date(val as string | number);
+		return isNaN(date.getTime()) ? val : date;
+	}
+
+	return [
+		Plot.ruleX(data, Plot.pointerX({ x, stroke: 'currentColor' })),
+		...axis.y.map((y, i) => Plot.dot(data, Plot.pointerX({ x, y, stroke: axis.z ?? colors[i] }))),
+		Plot.tip(
+			tipsData,
+			Plot.pointerX({ x, y: '_y', channels: channels(axis.y), format: { y: false } })
+		)
+	];
+}
+
+function avg(...args: Value[]) {
+	return args.reduce<number>((acc, k) => acc + Number(k), 0) / args.length;
+}
+
+function channels(y: string[]) {
+	return y.reduce<Record<string, string>>((acc, k) => ({ ...acc, [k]: k }), {});
 }
